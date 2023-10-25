a78f052894aac2176eee16d2bc27bd9edb0098f7
exception <erik@erosemberg.com>
2023-10-25 10:17:38 -0300

    chore: Env updates

--- src/env.mjs
+++ src/env.mjs
@@ -16,14 +16,6 @@ export const env = createEnv({
 
         CLOUDINARY_URL: z.string().min(1),
 
-        QSTASH_CURRENT_SIGNING_KEY: z.string().min(1),
-        QSTASH_NEXT_SIGNING_KEY: z.string().min(1),
-        QSTASH_CLIENT_TOKEN: z.string().min(1),
-        UPSTASH_REDIS_URL: z.string().optional(),
-        UPSTASH_REDIS_TOKEN: z.string().optional(),
-
-        RESEND_API_KEY: z.string().min(1),
-
         NGROK_URL: z.string().optional(),
 
         STRIPE_SECRET_KEY: z.string().min(1),
@@ -38,14 +30,8 @@ export const env = createEnv({
 
         CLOUDINARY_URL: process.env.CLOUDINARY_URL,
 
-        QSTASH_CURRENT_SIGNING_KEY: process.env.QSTASH_CURRENT_SIGNING_KEY,
-        QSTASH_NEXT_SIGNING_KEY: process.env.QSTASH_NEXT_SIGNING_KEY,
-        QSTASH_CLIENT_TOKEN: process.env.QSTASH_CLIENT_TOKEN,
         NGROK_URL: process.env.NGROK_URL,
-        RESEND_API_KEY: process.env.RESEND_API_KEY,
         STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
         STRIPE_ONE_TIME_PAYMENT_ID: process.env.STRIPE_ONE_TIME_PAYMENT_ID,
-        UPSTASH_REDIS_URL: process.env.UPSTASH_REDIS_URL,
-        UPSTASH_REDIS_TOKEN: process.env.UPSTASH_REDIS_TOKEN,
     },
 });
