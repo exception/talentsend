dfe85dd57cd3aba0a613fae0e3af9989a835bc94
exception <erik@erosemberg.com>
2023-10-25 23:05:12 -0300

    chore: Remove size

--- src/app/api/offer/og/route.tsx
+++ src/app/api/offer/og/route.tsx
@@ -7,11 +7,6 @@ const interBold = fetch(
 
 export const runtime = 'edge';
 
-export const size = {
-    width: 1200,
-    height: 630,
-};
-
 export const contentType = 'image/png';
 
 export async function GET(request: NextRequest) {
